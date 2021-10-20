730290909300c6a2a730867e7f07b2247fc6566d
Abdourahmane diallo <abdourahmane.benzy@gmail.com>
2021-10-20 17:15:07 +0200

    Initial com

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/header';
 import {BrowserRouter as Router, Route,Link,Switch,} from 'react-router-dom'
+import Welcome from './components/welcome';
 import Login from './components/login';
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
      <Router>					
 					<Switch>
 						<Route path='/' exact>
 							<Login />
 						</Route>
+						
 					</Switch>
 					
 						<Switch>
-							
+						<Route path='/welcome' exact>
+							<Sidebar />
+						
+						<Welcome />
+						</Route>
 						</Switch>
 						
 					
 			</Router>		
-    </div>
+    // </div>
+	
   );
 }
 
--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,29 +1,62 @@
-import React from 'react';
-import './index.css'
+import React,{useState} from 'react';
+import './index.css';
 
 const Login = () => {
-    return (
-  
-        <div className="form">
-            <h2>Login</h2>
-            <div className="input">
-                <div className="inputBox">
-                    <label for="">Username</label>
-                    <input type="text"/>
-                </div>
-                <div className="inputBox">
-                    <label for="">Password</label>
-                    <input type="password"/>
-                </div>
-                <div className="inputBox">
-                    <input type="submit" name="" value="Sign In" /> 
-                </div>
-            </div>
-            <p className="forgot">Forgot Password? <a href="#">Click Here</a></p>
-            
-        </div>
-  
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // setLoading(true);    
+        // try {
+        //   const result = await auth.signInWithEmailAndPassword(email, password);
+          
+        //   const { user } = result; 
+    
+        //    const logFirestore= await dbFirestore.doc(user.uid).set({email});
+          
+        //   console.log(logFirestore);
+    
+        //       dispatch({
+        //         type: "LOGGED_IN_USER",
+        //         payload: {
+        //           name:user.displayName ? user.displayName :user.email.split('@')[0],
+        //           email:user.email,
+        //         },
+        //       });
+        //    history.push("user/history")
+       
+    }
+
+    const handleChange= e=>{
+        setUsername(e.target.value)
+        console.log(username)
+    }
+        
+
+    return (
+      
+        <div className='main'>
+            <div className="form" onSubmit={handleSubmit} >
+                <h2>Login</h2>
+                <div className="input">
+                    <div className="inputBox">
+                        <label for="">Username</label>
+                        <input type="text" onChange={()=> handleChange}/>
+                    </div>
+                    <div className="inputBox">
+                        <label for="">Password</label>
+                        <input type="password" onChange={e=> setPassword(e.target.value)}/>
+                    </div>
+                    <div className="inputBox">
+                        <input type="submit" name="" value="Sign In" /> 
+                    </div>
+                </div>
+                <p className="forgot">Forgot Password? <a href="#">Click Here</a></p>
+                
+            </div>
+        </div>   
     )
 }
 
