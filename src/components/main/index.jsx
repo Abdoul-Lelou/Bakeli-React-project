ffb0d4932791ff053f89664d697c6ce4a806944e
Abdourahmane diallo <abdourahmane.benzy@gmail.com>
2021-10-21 21:22:58 +0200

    Initial com lastetsq

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,26 +1,14 @@
 import React,{useEffect,useState} from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import {dbCours, db} from "../../firebase";
+import {dbCours} from "../../firebase";
 import Swal from 'sweetalert2';
-// import withReactContent from 'sweetalert2-react-content';
-// import 'sweetalert2/src/sweetalert2.scss';
 import img1 from '../../images/img1.jpg'
-import Cours from '../cours';
 import './index.css';
 
 const Main = () => {
 
   const [dataCours, setDataCours] = useState([])
-  
-  // let items=['papa','mamn','koto','diadia','leyga','papa','mamn','koto','papa','mamn','koto','diadia','leyga','papa','mamn','koto','diadia','leyga','papa','mamn','koto','diadia','leyga']
-//   db.ref('/cours').on('value', function(snapshot) {
-//     snapshot.forEach(function(childSnapshot) {
-//       var childData = childSnapshot.val();
-//       // console.log(childData)
-//       datas.push(childData);
-//     });
-//      setDataCours(datas)
-// });
+
 
     useEffect(() => {
         dbCours.get().then((snapshot) => {
@@ -28,56 +16,11 @@ const Main = () => {
             id: doc.id,
             ...doc.data(),
           }));
-          console.log(data)
           setDataCours(data);
-          // setDataCours(data)
-          // [ { id: 'glMeZvPpTN1Ah31sKcnj', title: 'The Great Gatsby' } ]
         });
     }, [])        
 
-    const editCours=()=>{
 
-      // const id= this.ordonnanceDataForm.value.id;
-      // const ordonnance = {
-      //   medicament: this.ordonnanceDataForm.value.medicament,
-      //   dosage: this.ordonnanceDataForm.value.dosage,
-      //   quantite: this.ordonnanceDataForm.value.quantite
-      // }
-      Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 2000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-  
-      // this.auth.updateMedicament(id,ordonnance).subscribe(
-      //   data =>{
-      //     console.log(data);
-      //     this.ngOnInit();
-      //     Toast.fire({
-      //       icon:"success",
-      //       title: 'Modifié avec succès',
-      //       //imageUrl: 'https://i.imgur.com/4NZ6uLY.jpg'
-    
-      //     })       
-      //   },error=>{
-      //     Toast.fire({
-      //       icon:"error",
-      //       title: "Une erreur s'est produit",
-      //       //imageUrl: 'https://i.imgur.com/4NZ6uLY.jpg'
-    
-      //     })       
-      //   }
-      // );
-
-
-
-    }
 
 
     return (
@@ -129,7 +72,7 @@ const Main = () => {
                             <div className="card-body">
                               <p className="card-text">
                                 <small className="text-muted">
-                                  <button className='btn btn-outline-warning' title='edit' onClick={editCours}> <i class="fa fa-edit" aria-hidden="true"></i></button> &nbsp;
+                                  <button className='btn btn-outline-warning' title='edit' onClick=''> <i class="fa fa-edit" aria-hidden="true"></i></button> &nbsp;
                                   <button className='btn btn-outline-primary' title='archive'> <i class="fa fa-archive" aria-hidden="true"></i></button>&nbsp;
                                   <button className='btn btn-outline-success' title='detail'> <i class="fa fa-info-circle" aria-hidden="true"></i></button>
                                 </small>
